b152c5842cc7faaf0fb81868b5d52f20194792c3
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2024-03-06 14:42:22 +0000

    Update coursework.py

--- coursework.py
+++ coursework.py
@@ -1,7 +1,5 @@
 # In this section I am importing all the libraries I will need
 import math
-from math import *# In this section I am importing all the libraries I will need
-import math
 from math import *
 import numpy as np
 import matplotlib
@@ -18,18 +16,23 @@ yb = 1 # xb and yb give the coordinates of the upper right corner of the boundin
 hx = 0.04 # step sizes and end time
 hy = 0.04
 k = 0.02
-t_end = 3
+t_end = 30
 
 nx = int(xb / hx) + 1
 ny = int(yb / hy) + 1
 nt = int(t_end / k) + 1
 
-c = 1
+c = 0.1
 
 
 # In this section I am defining arrays I would need (if needed)
 
 U = np.ndarray((nt, nx, ny))
+
+x = np.arange(0,xb+hx,hx)  #meshgrid for surface
+y = np.arange(0,yb+hy,hy)
+
+Xg, Yg = np.meshgrid(x, y)
 
 # In this section I am setting the boundary conditions/initial values
 
@@ -37,8 +40,8 @@ U = np.ndarray((nt, nx, ny))
 
 U[0,:,:] = 0   # no initial displacement everywhere - boundary value in time
 U[1,:,:] = 0   # need another boundary condition for accel (should improve later!)
-U[:,:,0], U[:,:,-1] = 1, 0 # no displacement at the boundaries always
-U[:,0,:], U[:,-1,:] = 0, 0
+U[:,:,0], U[:,:,-1] = 0, 0 # no displacement at the boundaries always
+U[:,0,:], U[:,-1,:] = 0, 1
 
 
 #U[:,int(nx/2),int(ny/2)] = [3*sin(i) for i in range(nt)]   # oscillating point at the centre
@@ -59,7 +62,7 @@ for t in range(2, nt):
 
 norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
 
-#'''
+'''
 for i in range(int(t_end / k) + 1):
     plt.imshow(U[i], interpolation='bilinear', norm=norm)
     plt.colorbar()
@@ -68,73 +71,27 @@ for i in range(int(t_end / k) + 1):
     if keyboard.is_pressed('e'):  # if key 'q' is pressed 
             print('Abort!')
             break  # finishing the loop
-#'''
+'''
 
-# In this section I am celebrating
-print('CW done: I deserve a good mark')
-
-
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
+#testing surface plot
 
 
-
-# In this section I am setting the domain of solution and the discretised grid
-
-xb = 1
-yb = 1 # xb and yb give the coordinates of the upper right corner of the bounding rectangle, with the origin being the other
-
-hx = 0.01 # step sizes and end time
-hy = 0.01
-k = 0.01
-t_end = 3
-
-nx = int(xb / hx) + 1
-ny = int(yb / hy) + 1
-nt = int(t_end / k) + 1
-
-c = 0.5
-
-
-# In this section I am defining arrays I would need (if needed)
-
-U = np.ndarray((nt, nx, ny))
-
-# In this section I am setting the boundary conditions/initial values
-
-# first version: square box bounding values
-
-U[0,:,:] = 0   # no initial displacement everywhere - boundary value in time
-U[1,:,:] = 0   # need another boundary condition for accel (should improve later!)
-U[:,:,0], U[:,:,-1] = 1, 0 # no displacement at the boundaries always
-U[:,0,:], U[:,-1,:] = 0, 0
-
-#U[:,int(nx/2),int(ny/2)] = [1*sin(i) for i in range(nt)]   # oscillating point at the centre
-
-# In this section I am implementing the numerical method
-
-for t in range(2, nt):
-        for x in range(1, nx-1):#[i for i in range(1, nx - 1) if i != int(nx/2)]:
-            for y in range(1, ny-1):#[i for i in range(1, ny - 1) if i != int(ny/2)]:
-                uxx = (1/hx**2) * (U[t-1,x+1,y] - 2*U[t-1,x,y] + U[t-1,x-1,y])
-                uyy = (1/hy**2) * (U[t-1,x,y+1] - 2*U[t-1,x,y] + U[t-1,x,y-1])
-                U[t,x,y] = 2*U[t-1,x,y] - U[t-2,x,y] + k**2*c**2*uxx + k**2*c**2*uyy
-
-
-# In this section I am showing the results
-
-norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
 
 #'''
-for i in range(int(t_end / k) + 1):
-    plt.imshow(U[i], interpolation='bilinear', norm=norm)
-    plt.colorbar()
+ax = plt.axes(projection='3d')   # comment this away for contour plots
+for i in range(int(t_end / k) + 1):   
+    ax.plot_surface(Xg,Yg,U[i,:,:])
+    ax.plot_surface(Xg,Yg,U[i,:,:])
+    ax.set(xlim=(0, xb), ylim=(0, yb), zlim=(-3,3))
     plt.pause(0.000001)
-    plt.clf()
+    plt.cla()
+    if keyboard.is_pressed('e'):  # if key 'q' is pressed 
+            print('Abort!')
+            break  # finishing the loop
 #'''
+    
 
-print(U[:,int(nx/2)+1,int(ny/2)+1])
+
 
 # In this section I am celebrating
 print('CW done: I deserve a good mark')
